f02381172ed776a9c6a46c1f62504b9ca930f94b
Pratiksha0401 <pratiksha411998@gmail.com>
2025-06-22 23:34:10 +0530

    Role base authentication

---
+++ src/main/java/com/demo/journalApp/controller/AdminController.java
@@ -0,0 +1,40 @@
+package com.demo.journalApp.controller;
+
+import com.demo.journalApp.entity.User;
+import com.demo.journalApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    private UserService userService;
+
+//    @Autowired
+//    private AppCache appCache;
+
+    @GetMapping("/all-users")
+    public ResponseEntity<?> getAllUsers() {
+        List<User> all = userService.getAll();
+        if (all != null && !all.isEmpty()) {
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/create-admin-user")
+    public void createUser(@RequestBody User user) {
+        userService.saveAdmin(user);
+    }
+
+//    @GetMapping("clear-app-cache")
+//    public void clearAppCache(){
+//        appCache.init();
+//    }
+}
