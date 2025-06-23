cc50171511286aed31220a7ed5b82cec5944c654
Pratiksha0401 <pratiksha411998@gmail.com>
2025-06-23 23:52:44 +0530

    Application profiles

--- src/main/java/com/demo/journalApp/JournalApplication.java
+++ src/main/java/com/demo/journalApp/JournalApplication.java
@@ -3,18 +3,23 @@ package com.demo.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 @EnableTransactionManagement
 public class JournalApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JournalApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(JournalApplication.class, args);
+        String[] envs = applicationContext.getEnvironment().getActiveProfiles() ;
+        System.out.println(envs);
     }
 
     @Bean
